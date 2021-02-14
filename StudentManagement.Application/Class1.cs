8b4f2a31044b8f9e43b5252fdf220c8731ecc800
锦辉 宋 <sinno.song@outlook.com>
2021-02-14 16:29:56 +0800

    midify project some idle dictory

--- StudentManagement.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace StudentManagement.Application
-{
-    public class Class1
-    {
-    }
-}
--- StudentManagement.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace StudentManagement.Domain
-{
-    public class Class1
-    {
-    }
-}
